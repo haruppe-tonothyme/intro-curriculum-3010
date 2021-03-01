0eae02cb637c820904e3ff5917b76a2481f0b63a
haruppe-tonothyme <haruppe0413@gmail.com>
2021-03-01 20:24:32 +0900

    ファイルを削除してからテストを実行

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
+// テストの前に永続化されているファイルを消す
+const fs = require('fs');
+fs.unlink('./tasks.json',err => {
+    const todo = require('./index.js');
+})
+
+// add と list のテスト
+todo.add('ノートを買う');
+todo.add('鉛筆を買う');
 assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
 // done と donelist のテスト
